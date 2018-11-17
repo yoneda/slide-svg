01a4820520f77d19779c15e9e6018afc7ee51197
yoneda <nsshowdream@gmail.com>
2018-11-18 03:25:22 +0900

    add constants for basic slide application

--- src/constants/constants.js
+++ src/constants/constants.js
@@ -1,4 +1,6 @@
 
-export const ADD_COUNT = "ADD_COUNT";
-export const SUB_COUNT = "SUB_COUNT";
-export const RESET_COUNT = "RESET_COUNT";
+export const OPEN_MENU = "OPEN_MENU";
+export const CLOSE_MENU = "CLOSE_MENU";
+export const MOVE_NEXT_SLIDE = "MOVE_NEXT_SLIDE";
+export const MOVE_PREV_SLIDE = "MOVE_PREV_SLIDE";
+export const CONVERT_MARKDOWN_TO_HTML = "CONVERT_MARKDOWN_TO_HTML";
