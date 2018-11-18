5bbe4b4d3e0b4994ca869abcfe090f1d49d479ff
yoneda <nsshowdream@gmail.com>
2018-11-18 14:43:56 +0900

    make it possible to use redux dev tool

--- src/index.js
+++ src/index.js
@@ -6,7 +6,7 @@ import reducer from "./reducers/reducer.js";
 import { createStore } from "redux";
 import { Provider } from 'react-redux'
 
-const store = createStore(reducer);
+const store = createStore(reducer,window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__());
 
 render(
     <Provider store={store}>
