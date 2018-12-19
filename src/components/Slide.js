8431eabc6b7580a99f41813be88b86a20e2c4e32
yoneda <nsshowdream@gmail.com>
2018-12-19 22:53:12 +0900

    align-items: center;

--- src/components/Slide.js
+++ src/components/Slide.js
@@ -4,9 +4,9 @@ import styles from "./Slide.module.css";
 const Slide = ({ text, color }) => {
   const content = { __html: text };
   return (
-    <div className={styles.Slide}>
-      <div style={styles} dangerouslySetInnerHTML={content} />
-    </div>
+      <div className={styles.Slide}>
+      <div className={styles.Content} style={styles} dangerouslySetInnerHTML={content} />
+      </div>
   );
 };
 
